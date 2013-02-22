d8241363f61a098780e9922f845c477c72d4acf0
Philip Buchanan <pbuchanan@contextcreative.com>
2013-02-22 15:38:02 -0500

    Moved css and added initial colors.php logging file

---
+++ colors.php
@@ -0,0 +1,17 @@
+<?php
+
+function writeColors($newcolor) {
+
+	$currentlist = file_get_contents("colors.txt");
+	if ($newcolor == substr($currentlist, -6)) {}
+	elseif ($newcolor == substr($currentlist, -3)) {}
+	else {
+		$currentlist .= ', ' . $newcolor;
+		$fl = fopen("colors.txt", "w+");
+		fwrite($fl, $currentlist);
+	}
+	fclose($fl);
+
+}
+
+?>
