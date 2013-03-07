0d85714f58fa3d970a8dab68156aa5b9605eecbf
Philip Buchanan <pbuchanan@contextcreative.com>
2013-03-07 11:27:33 -0500

    Added color validation to global color save function

--- colors/colors.php
+++ colors/colors.php
@@ -4,17 +4,38 @@ if (isset($_GET['color'])) writeColors($_GET['color']);
 
 function writeColors($newcolor) {
 
-	$currentlist = file_get_contents("colors.txt");
+	if (validateColor($newcolor)) {
 	
-	if (strlen($currentlist) == 0) $sep = '';
-	else $sep =', ';
+		$currentlist = file_get_contents("colors.txt");
+		
+		if (strlen($currentlist) == 0) $sep = '';
+		else $sep =', ';
+		
+		if ($newcolor != substr($currentlist, -6) && $newcolor != substr($currentlist, -3)) {
+			$currentlist .= $sep . $newcolor;
+			$fl = fopen("colors.txt", "w+");
+			fwrite($fl, $currentlist);
+			fclose($fl);
+		}
 	
-	if ($newcolor != substr($currentlist, -6) && $newcolor != substr($currentlist, -3)) {
-		$currentlist .= $sep . $newcolor;
-		$fl = fopen("colors.txt", "w+");
-		fwrite($fl, $currentlist);
-		fclose($fl);
 	}
+
+}
+
+function validateColor($color) {
+
+	$regexs = array(
+		'/^[a-f0-9]{3}$/',
+		'/^[a-f0-9]{6}$/',
+		'/^#[a-f0-9]{3}$/',
+		'/^#[a-f0-9]{6}$/'
+	);
+	
+	foreach ($regexs as $regex) {
+		if (preg_match($regex, $color)) return true;
+	}
+	
+	return false;
 
 }
 
